68a9ed7864baac6323f540837149f2627882eaa4
LENNY\kayuyu <mwaurakayuyu@gmail.com>
2017-02-08 11:59:16 +0300

    comments

--- minmax.py
+++ minmax.py
@@ -1,9 +1,15 @@
+ 			
+#this code finds the min and max values using for loop
  def find_minmax(n):
- 	max = min = a[1]
- 	for (i = 2 to n):
+ 	max = min = a[1]  #assumes the first item in the array is min and max
+ 	#then it checks the second value in array and loops through to n
+ 	for (i = 2 to n): 
+ 		#if value in next array is geater than the current, max now becomes the value in next array
  		if  (a[1]>max):
  			then max = a[1]
+ 			#does the same for min and loops through the entire array 
  		else if (a[1]<min):
  			then min = a[1]
+ 			
 
  	
