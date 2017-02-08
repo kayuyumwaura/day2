8381f39ef70b6dff9dba51bdcc037d2ecffc3d47
LENNY\kayuyu <mwaurakayuyu@gmail.com>
2017-02-08 15:00:28 +0300

    some changes

--- word_count.py
+++ word_count.py
@@ -1,7 +1,10 @@
+#function to count the number of occurences a word makes in a sentence
 def word_count(word):
-	word_count = word.split()
+	word_count = word.split() 
 	word_count = {}
 	for word in word_count: 
 		word_count[word] +1
 	else:
 		word_count[word] = 1
+	for key, value in sorted(word_count.items(), key = operator.itemgeter(1)):
+		print ('key,value')
